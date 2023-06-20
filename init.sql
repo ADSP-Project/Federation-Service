fb69bb53d56eced8fea60ce151f1a2bdcf9320e8
Ali Aziri <66095628+freefreeP@users.noreply.github.com>
2023-06-20 20:46:08 +0000

    dockerfile for db and backend

---
+++ init.sql
@@ -0,0 +1,17 @@
+CREATE TABLE shops (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) UNIQUE,
+  webhookURL VARCHAR(255),
+  publicKey VARCHAR(1024)
+);
+
+CREATE TABLE partners (
+  shopId SERIAL PRIMARY KEY,
+  shopName VARCHAR(255),
+  canEarnCommission BOOLEAN,
+  canShareInventory BOOLEAN,
+  canShareData BOOLEAN,
+  canCoPromote BOOLEAN,
+  canSell BOOLEAN,
+  publicKey VARCHAR(1024)
+);
